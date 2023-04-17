922b13c5fa9518dec4ef061c66cd28cbc1eb4ec4
Lukas Risian <240556@vut.cz>
2023-04-17 10:15:22 +0200

    insertion_sort_OK

--- sorting.py
+++ sorting.py
@@ -28,7 +28,7 @@ def selection_sort(array, direction):
         for j in range(ind + 1, len(array)):
             if array[j] < array[min_index]:
                 min_index = j
-        (array[ind], array[min_index]) = (array[min_index], array[ind])
+        array[ind], array[min_index] = array[min_index], array[ind]
     if direction == "asc":
         return array
     elif direction == "desc":
@@ -36,12 +36,38 @@ def selection_sort(array, direction):
     else:
         return "Error in direction"
 
+def bubble_sort(arr):
+    swapped = False
+    for i in range(len(arr) - 1):
+        for j in range(0, len(arr) - i - 1):
+            if arr[j] > arr[j + 1]:
+                swapped = True
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+        if not swapped:
+            return arr
+    return arr
+
+def insertion_sort(arr):
+    if (n := len(arr)) <= 1:
+        return
+    for i in range(1, n):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
 
 def main():
     my_data = read_data("numbers.csv")
-    print(my_data["series_1"])
-    sorted_data = selection_sort(my_data["series_1"], "asc")
-    print(sorted_data)
+    print(my_data["series_1"], "Původné dáta")
+    sorted_data = selection_sort(my_data["series_1"].copy(), "asc")
+    print(sorted_data, "Selection sort")
+    bubble_sorted_data = bubble_sort(my_data["series_1"].copy())
+    print(bubble_sorted_data, "Bubble sort")
+    insertion_sort_data = insertion_sort(my_data["series_1"].copy())
+    print(insertion_sort_data, "Insertion sort")
     pass
 
 
